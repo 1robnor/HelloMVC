a7575c30af6510bb5cd8a106df1a8ffc66a98e7f
anmesk <an_mesk@live.com>
2020-02-11 13:42:50 -0500

    Fixed test dependency

--- HelloMVC/Controllers/HomeController.cs
+++ HelloMVC/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelloMVC.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
         public ViewResult Index()
         {
